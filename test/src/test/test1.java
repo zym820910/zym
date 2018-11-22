84a7969f6938f2148d78de4a7efc48e2d9e63def
zym820910 <44774139+zym820910@users.noreply.github.com>
2018-11-22 21:53:34 +0800

    Update test1.java

--- test/src/test/test1.java
+++ test/src/test/test1.java
@@ -8,9 +8,9 @@ public class test1 {
 		String providerTime = "2010-2-15";
 		String s= providerTime.substring(0,5) + "0" + providerTime.substring(5,9);
 		System.out.println(s);
-		System.out.println(s);
-		System.out.println(s);
-		System.out.println(s);
+		System.out.println(s3);
+		System.out.println(s1);
+		System.out.println(s2);
 		
 	}
 	
